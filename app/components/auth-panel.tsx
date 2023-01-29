1585e04b6f5370b1ce26e0fb4d65432a2f2ff066
effervescent000 <kislio01@gmail.com>
2023-01-29 11:30:50 -0500

    add auth panel to header

--- app/components/auth-panel.tsx
+++ app/components/auth-panel.tsx
@@ -8,22 +8,22 @@ const AuthPanel = () => {
     <div>
       {user ? (
         <Link
-          to="/notes"
-          className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+          to="/logout"
+          className="border-transparent hover:bg-yellow-50 flex items-center justify-center rounded-md border px-2 pb-1 text-base font-medium shadow-sm"
         >
-          View Notes for {user.email}
+          Logout
         </Link>
       ) : (
         <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
           <Link
             to="/join"
-            className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+            className="border-transparent bg-white text-yellow-700 hover:bg-yellow-50 flex items-center justify-center rounded-md border px-4 py-3 text-base font-medium shadow-sm sm:px-8"
           >
             Sign up
           </Link>
           <Link
             to="/login"
-            className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
+            className="bg-yellow-500 text-white hover:bg-yellow-600 flex items-center justify-center rounded-md px-4 py-3 font-medium"
           >
             Log In
           </Link>
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,9 +1,11 @@
+import AuthPanel from "./auth-panel";
 import Navbar from "./nav/navbar";
 
 function Header() {
   return (
     <div className="flex justify-between bg-dark-gray px-4 pt-4 pb-1">
       <Navbar />
+      <AuthPanel />
     </div>
   );
 }
